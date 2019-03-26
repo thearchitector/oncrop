726b495b995c840de1d7195126b4e4a23ca81deb
Elias Gabriel <egabriel@olin.edu>
2019-03-26 11:05:01 -0400

    Add module structure

--- source/app.py
+++ source/app.py
@@ -4,9 +4,9 @@ A web app interface for dynamic, live facial image insertion.
 @author: Elias Gabriel, Duncan Mazza
 @revision: v1.0
 """
-from flask render_template, Response
-from web_classes import WebApplication
-from cam_classes import CamReader
+from flask import render_template, Response
+from api.web_classes import WebApplication
+from api.cam_classes import CamReader
 
 
 def index():
@@ -24,8 +24,8 @@ def feed():
     """ Opens a camera reader, gets a processed frame, encodes it to JPEG, and returns it as a
     snippet of a multipart response body. """
     camera = CamReader()
-
-     while True:
+    
+    while True:
         frame = camera.get_frame()
         yield(b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
