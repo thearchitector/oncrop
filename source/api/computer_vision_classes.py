3f8d5fe24254b7ba5dc16dce710341338af00e65
Duncan Mazza <dmazza@olin.edu>
2019-03-26 12:37:21 -0400

    Add capability to detect aruco markers within class

--- source/api/computer_vision_classes.py
+++ source/api/computer_vision_classes.py
@@ -4,7 +4,7 @@ import os
 
 
 class ParseImage:
-    def __init__(self):
+    def __init__(self, source="webcam"):
         # Create aruco markers if necessary
         if os.path.exists("markers/"):
             "Markers already exist in markers/"
@@ -17,12 +17,32 @@ class ParseImage:
                 img = aruco.drawMarker(aruco_dict, marker_num, 700)
                 cv2.imwrite("markers/marker_{}.jpg".format(str(marker_num)), img)
 
-        # Set up the aruco-specific code
+        # Set up the aruco detection
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
         self.parameters = aruco.DetectorParameters_create()
-        
-    def
+
+        # Set up OpenCV
+        if source == "webcam":
+            self.cap = cv2.VideoCapture(0)
+        elif source == "Img":
+            pass
+            # TODO
+            # Add functionality for reading from an image instead of the webcam
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.aruco_dict, parameters=self.parameters)
+        gray = aruco.drawDetectedMarkers(gray, corners)
+        frame = aruco.drawDetectedMarkers(frame, corners)
+        # TODO: add capability for corner information to inform the placement of an image on top of the frame
+        return frame, gray
 
 
 if __name__ == "__main__":
-    write_aruco_code()
+    image_parser = ParseImage()
+    while True:
+        frame, gray = image_parser.get_frame()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
