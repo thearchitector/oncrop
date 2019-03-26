4b02df4b5b94cc079015ab64e4396d05af84e3c5
Elias Gabriel <egabriel@olin.edu>
2019-03-26 11:22:50 -0400

    Remove redundancies, again

--- source/blob_test.py
+++ source/blob_test.py
@@ -5,12 +5,12 @@ Prototyping for phone tracking with specific color-based markers.
 @revision: v1.1
 """
 import cv2
-import cam_classes as cl
+import api.cam_classes as cl
 
 if __name__ == "__main__":
     camera = cl.CamReader()
+    
     while True:
         frame = camera.get_frame()
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
