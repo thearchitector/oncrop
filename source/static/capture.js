1898ba45a281eaec338d145310a692e9b35d088d
Elias Gabriel <egabriel@olin.edu>
2019-04-01 22:17:39 -0400

    Fix capture orientation

--- source/static/capture.js
+++ source/static/capture.js
@@ -8,16 +8,16 @@ const ENDPOINT = '/capture';
 
 document.body.onkeyup = function(e) {
     if(e.keyCode == 32) {
-		let canvas = document.createElement('canvas');	
+		let canvas = document.createElement('canvas');
 		let im = document.getElementById("capture");
 		let bb = im.getBoundingClientRect();
 		canvas.width = bb.width;
 		canvas.height = bb.height;
-	
+
 		let context = canvas.getContext('2d');
-		context.drawImage(im, 0, 0, bb.width, bb.height);
 		context.scale(-1, 1);
-		
+		context.drawImage(im, 0, 0, bb.width, bb.height);
+
 		let data = new FormData();
 		data.append('URI', canvas.toDataURL('image/jpeg'));
 		fetch(ENDPOINT, {
